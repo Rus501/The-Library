ec2317bafda10241c83cbf65a7710225f0f1d1f2
unknown <rusonaldo@gmail.com>
2021-04-16 23:39:23 +0300

    Finished main functionality

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const addBook = document.querySelector('.plus_btn')
 const body = document.querySelector('body')
 const modal = document.querySelector('.modal')
-const closeModal = document.querySelector('.modal_close')
+const modalWindow = document.querySelector('.modal_window')
+const modalClose = document.querySelector('.modal_close')
 const modalSubmit = document.querySelector('#submit')
 const bookShelf = document.querySelector('.book_shelf')
 const bookExample = document.querySelector('.book')
@@ -9,51 +10,127 @@ const bookName = document.querySelector('#name')
 const bookAuthor = document.querySelector('#author')
 const bookPages = document.querySelector('#pages')
 const bookRead = document.querySelector('#read')
-
+let removeButtons = document.querySelectorAll('.book_remove')
+let readButtons = document.querySelectorAll('.book_read_btn')
+let data = 0
 
 function openModal() {
-    body.style.backgroundColor = 'rgba(0, 0, 0, 0.9)'
-    body.style.overflow = 'hidden'
-    modal.style.visibility = 'visible'
+  body.style.backgroundColor = 'rgba(0, 0, 0, 0.9)'
+  body.style.overflow = 'hidden'
+  modal.style.visibility = 'visible'
+  bookName.focus()
+  modal.addEventListener('click', checkClick);
 }
-addBook.onclick = () => openModal()
+addBook.onclick = openModal
 
-function closeModalWindow() {
-    body.style.backgroundColor = 'rgba(0, 0, 0, 0)'
-    body.style.overflow = 'visible'
-    modal.style.visibility = 'hidden'
+function closeModal() {
+  body.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+  body.style.overflow = 'visible'
+  modal.style.visibility = 'hidden'
+  modal.removeEventListener('click', checkClick)
 }
-closeModal.onclick = () => closeModalWindow()
+modalClose.onclick = closeModal
+
+function checkClick(event) {
+  modalWindow.contains(event.target) ? null : closeModal()
+}
 
 let myLibrary = []
 
-// function loopOverMyLibrary() {
-//     myLibrary.forEach(item => {
-//         item =
-//     })
-// }
-
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
 }
 
 function addBookToLibrary() {
-    const book = new Book(
-        bookName.value,
-        bookAuthor.value,
-        bookPages.value,
-        bookRead.value
-    )
-    myLibrary.push(book)
-    const newBook = bookExample.cloneNode(true)
-    newBook.querySelector('.book_name').innerHTML = book.title
-    newBook.querySelector('.book_author').innerHTML = book.author
-    newBook.querySelector('.book_pages').innerHTML = book.pages
-    bookShelf.appendChild(newBook)
-    closeModalWindow()
+  const book = new Book(
+    bookName.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookRead.checked
+  )
+  const newBook = bookExample.cloneNode(true)
+  newBook.setAttribute('data', data)
+  newBook.querySelector('.book_name').innerHTML = book.title
+  newBook.querySelector('.book_author').innerHTML = `By ${book.author}`
+  newBook.querySelector('.book_pages').innerHTML = `Pages: ${book.pages}`
+  newBook.querySelector('.book_read').innerHTML = `Read: ${book.read ? 'Yes' : 'No'}`
+  newBook.querySelector('.book_read_btn').innerHTML = book.read ? 'Unread' : 'Read'
+  myLibrary.push(book)
+  myLibrary.forEach(item => {item.data = item.data ?? data})
+  data++
+  bookShelf.appendChild(newBook)
+  removeButtons = document.querySelectorAll('.book_remove')
+  readButtons = document.querySelectorAll('.book_read_btn')
+  removeBook()  // redefine all remove buttons
+  readBook()    // redefine all read buttons
+  closeModal()
+  saveLocally()
 }
 
-modalSubmit.onclick = () => addBookToLibrary()
+function removeBook() {
+  removeButtons.forEach(item => {
+    item.onclick = function() {
+      this.parentNode.parentNode.remove()
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].data === parseInt(this.parentNode.parentNode.getAttribute('data'))) { myLibrary.splice(i, 1) }
+      }
+      saveLocally()
+    }
+  })
+}
+removeBook()
+
+function readBook() {
+  readButtons.forEach(item => {
+    item.onclick = function() {
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].data === parseInt(this.parentNode.parentNode.getAttribute('data'))) {
+          this.innerHTML === 'Unread' ? myLibrary[i].read = false : myLibrary[i].read = true
+        }
+      }
+      if (this.innerHTML === 'Unread') {
+        this.innerHTML = 'Read'
+        this.parentNode.parentNode.querySelector('.book_read').innerHTML = 'Read: No'
+      } else {
+        this.innerHTML = 'Unread'
+        this.parentNode.parentNode.querySelector('.book_read').innerHTML = 'Read: Yes'
+      }
+      saveLocally()
+    }
+  })
+}
+readBook()
+
+// Local Storage
+function saveLocally() {
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+}
+
+function restoreLocally() {
+  myLibrary = JSON.parse(localStorage.getItem('myLibrary'))
+  if (myLibrary === null) myLibrary = []
+  if (myLibrary.length > 0) {
+    data = myLibrary[myLibrary.length - 1].data + 1
+  } else { data = 0 }
+  myLibrary.forEach(item => {
+    const newBook = bookExample.cloneNode(true)
+    newBook.setAttribute('data', item.data)
+    newBook.querySelector('.book_name').innerHTML = item.title
+    newBook.querySelector('.book_author').innerHTML = `By ${item.author}`
+    newBook.querySelector('.book_pages').innerHTML = `Pages: ${item.pages}`
+    newBook.querySelector('.book_read').innerHTML = `Read: ${item.read ? 'Yes' : 'No'}`
+    newBook.querySelector('.book_read_btn').innerHTML = item.read ? 'Unread' : 'Read'
+    myLibrary.forEach(item => {item.data = item.data ?? data++})
+    bookShelf.appendChild(newBook)
+    removeButtons = document.querySelectorAll('.book_remove')
+    readButtons = document.querySelectorAll('.book_read_btn')
+    removeBook()  // redefine all remove buttons
+    readBook()    // redefine all read buttons
+    closeModal()
+  })
+}
+// localStorage.clear()
+restoreLocally()
